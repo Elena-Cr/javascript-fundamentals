45e67dda99a5ddd502befa8596d2fb2ea67d0f0a
Elena Cristescu <elenacristescu11@gmail.com>
2023-08-19 16:57:41 +0200

    switch statement example

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-const inputYear = "1991";
-console.log(Number(inputYear));
-console.log(Number(inputYear) + 18);
+const day = "wednesday";
+
+switch (day) {
+  case "monday":
+    console.log("Plan course statement");
+    console.log("Go to coding meetup");
+    break;
+  case "tuesday":
+    console.log("Prepare theory videos");
+    break;
+  case "wednesday":
+  case "thursday":
+    console.log("Write code examples");
+    break;
+  case "friday":
+    console.log("Record video");
+    break;
+  case "saturday":
+  case "sunday":
+    console.log("Enjoy the weekend");
+    break;
+  default:
+    console.log("Not a valid day!");
+}
