d2a6599ec63f124ea00af1513eef00b6537836c1
Elena Cristescu <elenacristescu11@gmail.com>
2023-08-19 16:16:57 +0200

    try type conversion

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-const age = 14;
-
-if (age >= 18) {
-  console.log(`Sarah can start driving licence `);
-} else {
-  console.log(`Sarah is not old enough; she needs to wait ${18 - age} years`);
-}
-
-const birthYear = 1991;
-let century;
-if (birthYear <= 2000) {
-  century = 20;
-} else {
-  century = 21;
-}
-console.log(century);
+const inputYear = "1991";
+console.log(Number(inputYear));
+console.log(Number(inputYear) + 18);
