85cec9bedbee7d40452cfec77c7a6d6f8b072985
Elena Cristescu <elenacristescu11@gmail.com>
2023-08-20 17:15:08 +0200

    create arrow functions

--- part-2/script.js
+++ part-2/script.js
@@ -15,15 +15,28 @@
 // const appleOrangeJuice = fruitProcessor(2, 4);
 // console.log(appleOrangeJuice);
 
-function calcAge1(birthYear) {
-  return 2023 - birthYear;
-}
+// function calcAge1(birthYear) {
+//   return 2023 - birthYear;
+// }
 
-const age1 = calcAge1(1991);
+// const age1 = calcAge1(1991);
 
-const calcAge2 = function (birthYear) {
-  return 2023 - birthYear;
+// const calcAge2 = function (birthYear) {
+//   return 2023 - birthYear;
+// };
+
+// const age2 = calcAge2(1991);
+// console.log(age1, age2);
+
+const calcAge3 = (birtYear) => 2023 - birtYear;
+
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear) => {
+  const age = calcAge3(birthYear);
+  const retirement = 65 - age;
+  return retirement;
 };
 
-const age2 = calcAge2(1991);
-console.log(age1, age2);
+console.log(yearsUntilRetirement(1991));
