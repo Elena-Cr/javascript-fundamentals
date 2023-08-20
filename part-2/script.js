8d654511d4940c152fe9364a0682881eecd2703c
Elena Cristescu <elenacristescu11@gmail.com>
2023-08-20 17:07:18 +0200

    create a function declaration and a function expression

--- part-2/script.js
+++ part-2/script.js
@@ -1,16 +1,29 @@
 "use strict";
-function logger() {
-  console.log("My name is Elena");
+// function logger() {
+//   console.log("My name is Elena");
+// }
+
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+//   return juice;
+// }
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+// console.log(fruitProcessor(5, 0));
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+function calcAge1(birthYear) {
+  return 2023 - birthYear;
 }
 
-logger();
+const age1 = calcAge1(1991);
 
-function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
-  return juice;
-}
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
-console.log(fruitProcessor(5, 0));
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
+const calcAge2 = function (birthYear) {
+  return 2023 - birthYear;
+};
+
+const age2 = calcAge2(1991);
+console.log(age1, age2);
