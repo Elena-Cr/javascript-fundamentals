3332c8e667821c71cecc394428d87acfcd7a0733
Elena Cristescu <elenacristescu11@gmail.com>
2023-08-22 15:19:50 +0200

    use loops with arrays, break and continue

--- part-2/script.js
+++ part-2/script.js
@@ -105,6 +105,45 @@
 
 // elena.location = "DK";
 
-for (let rep = 1; rep <= 10; rep++) {
-  console.log(`Lifting weights, repetition ${rep} !`);
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`Lifting weights, repetition ${rep} !`);
+// }
+
+const jonas = [
+  "Jonas",
+  "Schmedtmann",
+  2037 - 1991,
+  "teacher",
+  ["Michael", "Peter", "Steven"],
+];
+
+const types = [];
+
+for (let i = 0; i < jonas.length; i++) {
+  console.log(jonas[i]);
+
+  // types[i] = typeof jonas[i];
+  types.push(typeof jonas[i]);
+}
+console.log(types);
+
+const years = [1991, 1995, 2008, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+  ages[i] = 2023 - years[i];
+}
+console.log(years);
+console.log(ages);
+
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] !== "string") continue;
+
+  console.log(jonas[i]);
+}
+
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] === "number") break;
+
+  console.log(jonas[i]);
 }
